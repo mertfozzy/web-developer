e0b58e317082b2543f2d2ce66c0c449f020a7472
Mert Altuntaş <mertfozzy@gmail.com>
2021-09-01 12:13:39 +0300

    01.09.2021

---
+++ Javascript/code11-domProject1/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+
+eventListener();
+
+function eventListener() {
+    form.addEventListener("submit", addNewItem);
+}
+
+function addNewItem(e) {
+    
+    if (input.value === " ") {
+        alert("You didn't type anything.")
+    }
+
+    const li = document.createElement("li");
+    li.className = "list-group-item list-group-item-secondary"; 
+    li.appendChild(document.createTextNode(input.value));
+
+    const a = document.createElement("a");
+    a.classList = "delete-item float-right";
+    a.setAttribute("href", "#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    li.appendChild(a); //add a to li
+    taskList.appendChild(li); // add li to ul
+    input.value = ''; //clear input
+
+
+    console.log(li);
+
+    e.preventDefault();
+
+}
