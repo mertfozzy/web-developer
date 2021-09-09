11cd0c8aefa7ab6baac98efc98343d3969e11c46
Mert Altuntaş <mertfozzy@gmail.com>
2021-09-09 23:16:03 +0300

    09.09.2021

--- Javascript/code12-domProject2/script.js
+++ Javascript/code12-domProject2/script.js
@@ -25,8 +25,31 @@ var models = [
     }
 ];
 
-index = 1;
+var index = 0;
+var counter = models.length;
+showSlide(index);
 
-document.querySelector(".card-img-top").setAttribute("src", models[index].image);
-document.querySelector(".card-title").textContent = models[index].name;
-document.querySelector(".card-link").setAttribute("href", models[index].link);
+
+document.querySelector(".fa-arrow-circle-left").addEventListener("click",function(params) {
+    
+    index--;
+    showSlide(index); //azaltma veya artırmadan sonra tekrar indexi çekiyoruz
+    console.log(index);
+
+});
+
+document.querySelector(".fa-arrow-circle-right").addEventListener("click",function(params) {
+    
+    index++;
+    showSlide(index); //azaltma veya artırmadan sonra tekrar indexi çekiyoruz
+    console.log(index);
+
+});
+
+function showSlide(index) {
+    document.querySelector(".card-img-top").setAttribute("src", models[index].image);
+    document.querySelector(".card-title").textContent = models[index].name;
+    document.querySelector(".card-link").setAttribute("href", models[index].link);
+
+}
+
