339d8a2dfb38e4aee80be21c620e0769d999e967
Mert Altuntaş <mertfozzy@gmail.com>
2021-09-09 22:54:16 +0300

    09.09.2021

---
+++ Javascript/code12-domProject2/script.js
@@ -0,0 +1,32 @@
+var models = [
+    {
+        name : "Bmw 418d",
+        image : "img/bmw.jpg",
+        link : "http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe"
+
+    },
+    {
+        name : "Honda City",
+        image : "img/honda.jpg",
+        link : "http://www.arabalar.com.tr/honda/city/2021/1-5-elegance-cvt"
+
+    },
+    {
+        name : "Dacia Duster",
+        image : "img/dacia.jpg",
+        link : "http://www.arabalar.com.tr/dacia/duster/2021/1-0-comfort"
+
+    },
+    {
+        name : "VW Passat",
+        image : "img/passat.jpg",
+        link : "http://www.arabalar.com.tr/volkswagen/passat/2021/1-5-business-dsg"
+
+    }
+];
+
+index = 1;
+
+document.querySelector(".card-img-top").setAttribute("src", models[index].image);
+document.querySelector(".card-title").textContent = models[index].name;
+document.querySelector(".card-link").setAttribute("href", models[index].link);
